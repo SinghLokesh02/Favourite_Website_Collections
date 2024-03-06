8608b6ea686a17a2cf0dda3d39550abd2351d601
Lokesh <lokeshsingh7695@gmail.com>
2024-03-06 18:39:30 +0530

    Added Modals for Popup while copy link of Website

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-let button = document.querySelectorAll("button");
-
+let button = document.querySelectorAll(".link-btn");
+const modal = document.querySelector("#modal");
+const modalCopy = document.querySelector("#modalCopy");
+const list = document.querySelector("li");
+const copyBtn = document.querySelector(".copy-button");
+let link = "";
 button.forEach((element) => {
   element.addEventListener("click", () => {
     link = element.parentElement.previousElementSibling.href;
-    confirm(link);
+    list.innerText = `${link}`;
+    modal.showModal();
   });
+});
+const closeModal = document.querySelector(".close-button");
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard
+    .writeText(link)
+    .then(() => {
+      modalCopy.showModal();
+      setTimeout(() => {
+        modalCopy.close();
+      }, 1500);
+    })
+    .catch((error) => {
+      console.error("Failed to copy link: ", error);
+    });
 });
